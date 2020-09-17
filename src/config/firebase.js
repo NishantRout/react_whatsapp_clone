fa211726f7c05534057f3215c4acda46f8dc4a5e
NishantRout <48598347+NishantRout@users.noreply.github.com>
2020-09-18 00:21:42 +0530

    cleanup process

---
+++ src/config/firebase.js
@@ -0,0 +1,11 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyAcu-lUnmYqfKpqAWu3LR2SZ-BFt8Dt30w",
+    authDomain: "react-whatsapp-clone-5b73f.firebaseapp.com",
+    databaseURL: "https://react-whatsapp-clone-5b73f.firebaseio.com",
+    projectId: "react-whatsapp-clone-5b73f",
+    storageBucket: "react-whatsapp-clone-5b73f.appspot.com",
+    messagingSenderId: "426195343178",
+    appId: "1:426195343178:web:6462f21353852716ab53b3",
+    measurementId: "G-73K6KEVWK5"
+};
